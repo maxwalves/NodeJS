9b82e465dccabcd8352d36b3c68f3be7f3716756
Max William Alves <maxwilliamalves@gmail.com>
2023-03-07 10:24:13 -0300

    Enviando projeto para o GitHub

---
+++ process.js
@@ -0,0 +1,7 @@
+//console.log('Seu nome é', process.argv[2] + ' ' + process.argv[3])
+
+//const firstName = process.argv[2]
+//const lastName = process.argv[3]
+
+//console.log(`Seu nome é ${firstName} ${lastName}`)
+console.log(process.argv)
---
+++ require.js
@@ -0,0 +1,6 @@
+//const path = require('path');
+//console.log(path.basename(__filename));
+
+//MEUS MÓDULOS
+const myModule = require('./exports.js')
+console.log(myModule);
